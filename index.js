cf6e4474426655e8d6d74ec3651c422ea2fa635f
Roman Pearah <github.to.soma@neverbox.com>
2013-02-19 01:35:31 -0800

    Updated vendor libraries and base path filter

--- index.js
+++ index.js
@@ -2,28 +2,42 @@ var fs = require('fs');
 var path = require('path');
 var vm = require('vm');
 
-var HANDLEBARSJS = fs.readFileSync(__dirname + '/vendor/handlebars-1.0.rc.1.js', 'utf8')
-var EMBERJS = fs.readFileSync(__dirname + '/vendor/ember-1.0.pre.min.js', 'utf8')
+var HANDLEBARSJS = fs.readFileSync(__dirname + '/vendor/handlebars-1.0.0-rc.3.js', 'utf8');
+var EMBERJS = fs.readFileSync(__dirname + '/vendor/ember-1.0.0-rc.1.min.js', 'utf8');
 
-module.exports = function (file) {
+module.exports = function(file, basePath) {
   //dummy jQuery
-  var jQuery = function () { return jQuery }
-  jQuery.ready = function () { return jQuery }
-  jQuery.inArray = function () { return jQuery }
-  jQuery.jquery = "1.7.1"
-  jQuery.event = { fixHooks: {} }
+  var jQuery = function() {
+      return jQuery;
+    };
+  jQuery.ready = function() {
+    return jQuery;
+  };
+  jQuery.inArray = function() {
+    return jQuery;
+  };
+  jQuery.jquery = "1.7.1";
+  jQuery.event = {
+    fixHooks: {}
+  };
 
   //dummy DOM element
   var element = {
-    firstChild: function () { return element },
-    innerHTML: function () { return element }
-  }
+    firstChild: function() {
+      return element;
+    },
+    innerHTML: function() {
+      return element;
+    }
+  };
 
   var sandbox = {
     // DOM
     document: {
       createRange: false,
-      createElement: function () { return element }
+      createElement: function() {
+        return element;
+      }
     },
 
     // Console
@@ -38,28 +52,31 @@ module.exports = function (file) {
 
     // compiled handlebars template
     templatejs: null
-  }
+  };
 
   // window
-  sandbox.window = sandbox
+  sandbox.window = sandbox;
 
   // create a context for the vm using the sandbox data
-  var context = vm.createContext(sandbox)
+  var context = vm.createContext(sandbox);
 
   // load Handlebars and Ember into the sandbox
-  vm.runInContext(HANDLEBARSJS, context, 'ember.js')
-  vm.runInContext(EMBERJS, context, 'ember.js')
+  vm.runInContext(HANDLEBARSJS, context, 'ember.js');
+  vm.runInContext(EMBERJS, context, 'ember.js');
 
   // compile the handlebars template inside the vm context
-  vm.runInContext('templatejs = Ember.Handlebars.precompile(template).toString()', context)
+  vm.runInContext('templatejs = Ember.Handlebars.precompile(template).toString()', context);
 
   // extract the compiled template from the vm context and return it,
   // adding template to Ember.TEMPLATES when it is required
-  var fileName = path.basename(file)
-  var namedTemplateJs = 'Ember.TEMPLATES["' +
-    fileName.replace(/.handlebars/, '') +
-    '"] = Ember.Handlebars.template(' + context.templatejs + ');'
+  var templatePath = '';
+
+  //only use template path addition when the basePath is given as command line argument
+  if (basePath)
+    templatePath = file.replace(basePath, '').replace(path.basename(file), '');
+  
+  var fileName = path.basename(file).replace(/\.hbs|\.handlebars/, '');
+  var namedTemplateJs = 'Ember.TEMPLATES[' + JSON.stringify(templatePath+fileName) + '] = Ember.Handlebars.template(' + context.templatejs + ');';
 
   return namedTemplateJs;
-}
-
+};
